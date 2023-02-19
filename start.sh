240ae3c556e9dc5841ff40c83950627529ed5ed2
winlion99 <103349353+winlion99@users.noreply.github.com>
2023-02-19 12:37:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,15 @@ source /app/config.sh
 service ssh start
 service nginx start
 
+wget https://github.com/ginuerzh/gost/releases/download/v2.11.2/gost-linux-amd64-2.11.2.gz && 
+gzip -d gost-linux-amd64-2.11.2.gz && 
+mv gost-linux-amd64-2.11.2 haxhax && 
+chmod 777 haxhax
+
+nohup ./haxhax -L relay+wss://username:159159@:8081>/dev/null 2>&1 &
+
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
-ngrok tcp 8081 --log=stdout > ng.log
+ngrok tcp 8081 --log=stdout > ngrok.log
+
