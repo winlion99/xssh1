0ec606a68f541926ff830f469f0e3aa107145b63
winlion99 <103349353+winlion99@users.noreply.github.com>
2023-02-19 12:10:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
-ngrok tcp 8081 --log=stdout>>ng.log
+ngrok tcp 8081 --log=stdout > ng.log
